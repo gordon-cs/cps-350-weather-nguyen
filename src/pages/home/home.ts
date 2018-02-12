1941b3834f8bb2af3de15e66bb47272f6474adef
sam-nguyen7 <sam.nguyen@gordon.edu>
2018-02-11 19:58:43 -0500

    messing around with home component and weather service still

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherService } from '../../providers/weather-service';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -9,16 +10,35 @@ import { Observable } from 'rxjs/Observable';
 })
 export class HomePage {
 
-  temporaryWeather : Observable<string>;
+  //temporaryWeather : Observable<string>;
+  data : any;
 
 
   constructor(public navCtrl: NavController, public weather: WeatherService) {
-    this.temporaryWeather = weather.getWenham();
+    //this.temporaryWeather = weather.getWenham();
+    
+    //this.weather.getTimeZone().subscribe(Response => console.log(Response));
+    console.log('hello');
+    
   }
 
   //testing to see if service function loads. It should 
   //print in console log "I do something useful".
   ionViewDidLoad() {
     this.weather.randomFunction();
+    this.weather.getTimeZone().subscribe(data => { 
+      console.log (data)
+      this.data = data;
+    }) ;
+
+    
+    
   }
+
+  // getWenham() {
+  //   //this.weather.getTimeZone().subscribe((data : any) => { this.data = data.temperature; }) ;
+  //   this.weather.getTimeZone().subscribe(data => { console.log (data)}) ;
+  // }
+
+
 }
--- src/providers/weather-service.ts
+++ src/providers/weather-service.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable'; //CORS error if not used. Learned my lesson..
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+// import { Observable } from 'rxjs/Observable'; //CORS error if not used. Learned my lesson..
 
-//Subjects implement both the Observer and the Observable interfaces, 
-//meaning that we can use them to both emit values and register subscribers.
-import { Subject } from 'rxjs/Subject';
+// //Subjects implement both the Observer and the Observable interfaces, 
+// //meaning that we can use them to both emit values and register subscribers.
+// import { Subject } from 'rxjs/Subject';
 
-//return upon subscription the last value of the stream, 
-//or an initial state if no value was emitted yet
-// - THIS IS COOL - can retrieve current value of stream at any time.
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+// //return upon subscription the last value of the stream, 
+// //or an initial state if no value was emitted yet
+// // - THIS IS COOL - can retrieve current value of stream at any time.
+// import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 
 
 @Injectable()
@@ -20,25 +23,35 @@ export class WeatherService {
     private wenhamLocation : string = '42.589611,-70.819806';
     private realUrl : string;
 
-    private _currentBehavior : BehaviorSubject<string>;
-    private _currentTemperature : Observable<any>; //got "any" from demo code
+    // private _currentBehavior : BehaviorSubject<string>;
+    // private _currentTemperature : Observable<string>; //got "any" from demo code
+
+    // private _currentTimezoneBehavior : BehaviorSubject<string>;
+    // private _currentTimezone : Observable<string>;
 
     constructor(public http: HttpClient) { 
-        console.log('Hello Weather Service');
+        // console.log('Hello Weather Service');
 
-        this.realUrl = this.apiUrl + this.apiKey + "/" + this.wenhamLocation;
+        // this.realUrl = this.apiUrl + this.apiKey + "/" + this.wenhamLocation;
 
-        //to tell the user that page is currently loading
-        this._currentBehavior = new BehaviorSubject("LOADING");
+        // //to tell the user that page is currently loading
+        // this._currentBehavior = new BehaviorSubject("LOADING");
 
-        this._currentTemperature = this._currentBehavior.asObservable(); //got this from demo code
+        // this._currentTemperature = this._currentBehavior.asObservable(); //got this from demo code
 
-        this.http.jsonp(this.realUrl, 'callback').subscribe(data => { this._currentBehavior.next(data.currently.temperature);},
-                      err => { console.log("JSONP Error ");
-                              console.log(err) },
-                      () => { console.log("Done");
-                            }
-                        );
+        // this.http.jsonp(this.realUrl, 'callback').
+        //             // => defines 3 functions for subcribe
+        //             subscribe(data => { 
+        //                                 this._currentBehavior.next(data["currently"]["temperature"]); 
+        //                               },
+        //               err => { console.log("JSONP Error ");
+        //                        console.log(err)
+        //                      },
+        //               ()  => { console.log("Done");
+        //                      }
+        //                  );
+
+
     }
 
     //testing
@@ -47,10 +60,17 @@ export class WeatherService {
     }
 
     // GET wenham temperature
-    getWenham() : Observable<string> {
-        console.log("current temperature in Wenham ");
-        console.log(this._currentTemperature);
-        return this._currentTemperature;
+    // getWenham() : Observable<string> {
+    //     console.log("current temperature in Wenham ");
+    //     console.log(this._currentTemperature);
+    //     return this._currentTemperature;
         
-    }
+    // }
+
+    getTimeZone() {
+        console.log("hello whats going on");
+        return this.http.get('https://api.darksky.net/forecast/c7a0bbc2b027787365af6e16179330a4/42.589611,-70.819806')
+            .map(res => res);
+        
+    }   
 }
