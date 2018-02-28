e86702986446efa87835971c11581d1157806387
sam-nguyen7 <sam.nguyen@gordon.edu>
2018-02-27 20:21:05 -0500

    finished 7 days forecast. still no icons

--- src/pages/forecast/forecast.ts
+++ src/pages/forecast/forecast.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController} from 'ionic-angular';
 import { WeatherService } from '../../providers/weather-service';
 import { Observable } from 'rxjs/Observable';
-import { Temperature } from '../../models/temperature.model'; 
+import { Temperature, Daily } from '../../models/temperature.model';
+import { dayforecast } from '../../models/forecast.model';
+
+//got from Max, implemented in forecast service
+import { LoadingController } from 'ionic-angular'; 
+ 
 
 
 @Component({
@@ -12,21 +17,59 @@ import { Temperature } from '../../models/temperature.model';
 export class ForecastPage {
 
   temperature: Observable<Temperature>;
-  private currentTemp: string;
-  private currentSum: string;
+  dailyTemp: Daily;
+  sevenDays: dayforecast[];
 
-  constructor(public navCtrl: NavController, public weather: WeatherService) {
-    this.getTemperature();
+  constructor(public navCtrl: NavController, public weather: WeatherService, private loadingCtrl: LoadingController) {
+
+      this.sevenDays = [ new dayforecast(),new dayforecast(),new dayforecast(),
+                         new dayforecast(),new dayforecast(),new dayforecast(),
+                         new dayforecast()];
+      this.getWeek();
+      this.sevenDays[0].setWeekday("Today");
+      this.loadWeek();
+  }
+
+//converting numbers in API to date format
+//Max wrote this function as well as loadWeek function
+//Link i researched: https://ionicframework.com/docs/api/components/loading/LoadingController/
+
+getWeek() {
+  let todayNum = new Date().getDay();
+  let i = 0;
+  this.sevenDays.forEach(day => 
+    { day.setWeekday(this.weather.divideDay(todayNum + i)); 
+      i++;}
+    );
+}
+
+//load the week
+loadWeek() {
+  let loading = this.loadingCtrl.create({
+    content: ''
+  });
+
+  loading.present();
+
+  setTimeout(() => {
+    this.getTemperature()
+    loading.dismiss();
+  }, 150);//may need to change
 }
 
 getTemperature() {
   this.temperature = this.weather.getDefaultTemperature();
-  this.temperature.subscribe( (data: any) => { 
+  this.temperature.subscribe( (data : any) => { 
+    this.dailyTemp = data.daily; 
     if (data.currently != undefined )
     {
-      this.currentTemp = data.currently.temperature;
-      this.currentSum = data.currently.summary;
+      let i = 0;
 
+      this.sevenDays.forEach(day => {
+        day.setTempHigh(this.dailyTemp.data[i]["temperatureHigh"]);
+        day.setTempLow(this.dailyTemp.data[i]["temperatureLow"]);
+        day.setSummary(this.dailyTemp.data[i]["summary"]);
+      })
     } 
   })
 }
